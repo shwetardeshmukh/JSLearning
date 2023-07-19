1ae0247dfeb4d56ff6b423ee7946f115859f02c3
shwetardeshmukh <shwetardeshmukh1111@gmail.com>
2023-07-19 21:11:22 +0530

    JS basics learning
    JS basics learning

---
+++ 01_variableAssign.js
@@ -0,0 +1,59 @@
+// var myFullName;
+// console.log( "Befor initilization: " +myFullName);
+// myFullName ="Shweta Deshmukh";
+// console.log("After initialization:"+ myFullName);
+// myFullName = "Shweta Raosaheb Deshmukh";
+// console.log("updated value :" + myFullName);
+// console.log("---------------------------------------------------------------------------------------");
+// myLove = "I love only JS";
+// console.log("myLove value : " + myLove);
+// console.log("-------------------------------------------------------------------------------");
+// firstName = "Shweta";
+// console.log("First Name  : "+ firstName);
+// lastName = "Deshmukh";
+// console.log("Last Name   : " +lastName);
+// age = 23;
+// console.log("Age         : "+age);
+// address = "Sangola";
+// console.log("Address     : " +address);
+// pinCode =" 413307";
+// console.log("Pincode     : "+pinCode);
+// gender = "Female";
+// console.log("Gender      : "  +gender);
+// isMarried = "false";
+// console.log("is Married  : "  +  isMarried );       
+
+var myFullName;
+myFullName
+console.log(myFullName);
+var myFullName;
+myFullName = "Shweta Deshmukh"
+console.log(myFullName);
+var myFullName;
+myFullName = "Shweta Raosaheb Deshmukh "
+console.log(myFullName);
+console.log("---------------------------------------------------------------------------------");
+var myLOve = "I love only JS"
+console.log( myLOve);
+console.log("------------------------------------------------------------------------------------");
+var firstName = "Shweta"
+console.log(firstName );
+var lastName = "Deshmukh"
+console.log(lastName );
+var age = "23"
+console.log(age);
+var address= "Sangola"
+console.log(address);
+
+var pinCode = "413307"
+console.log(pinCode );
+var gender= "Female"
+console.log(gender );
+var isMarried= "False"
+console.log(isMarried );
+
+
+
+
+
+
---
+++ hello.js
@@ -0,0 +1,26 @@
+var myFirstName; // Variable Declaration
+myName = "Shweta"; // Variable Assignment or Initialization
+
+console.log(myName);
+
+var city = "Pune"; // Variable Declaration and Initialization
+console.log(city);
+
+var pin; // Variable Declaration
+console.log(pin);
+var firstName = "Gajanan";
+console.log(firstName);
+console.log(typeof firstName);
+
+var age = 32;
+console.log(age);
+console.log(typeof age);
+
+var isMarried = true;
+console.log(isMarried);
+console.log(typeof isMarried);
+
+var city = 'Pune';
+console.log(typeof city);
+city = 50000;
+console.log(typeof city);
