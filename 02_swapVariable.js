de54377ccfa419569c144dd3f9ade1f6e3e283dc
shwetardeshmukh <shwetardeshmukh1111@gmail.com>
2023-07-20 16:16:07 +0530

    datatype ,swap and function changes commited and push
    datatype ,swap and function changes commited and push

---
+++ 02_swapVariable.js
@@ -0,0 +1,20 @@
+var name1 , name2;
+name1 = "sweety";
+name2 = "cutie";
+console.log("Before Swap : Name1 -" ,name1,", Name2 -", name2);
+var temp = name1;
+name1 = name2;
+name2 = temp;
+console.log("After Swap : Name1-" ,name1,", Name2 -", name2);
+console.log("-----------------------------------------------------------------");
+var num1,num2,num3;
+num1 = "100";
+num2 = "200";
+num3 = "300";
+console.log("Before Swap : num1 -",num1," ,num2 -",num2," ,num3 -",num3);
+var temp =num1;
+num1 =num2;
+num2 =num3;
+num3 = temp;
+console.log("After Swap : num1 -" ,num1,",num2 -",num2,",num3 -",num3);
+
