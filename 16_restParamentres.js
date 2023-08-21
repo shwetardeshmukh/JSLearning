633e5282fbfbef647b68fdf06573418fad9901d7
shwetardeshmukh <shwetardeshmukh1111@gmail.com>
2023-08-21 10:46:36 +0530

    rest parametres assignment completed
    rest parametres assignment completed

---
+++ 16_restParamentres.js
@@ -0,0 +1,15 @@
+console.log("-----------------------------------Assignment 21-8-2023------------------------------------------------------");
+function add (num1 ,num2 ,...num){
+    let sum=num1+num2;
+    for (let index = 0; index < num.length; index++) {
+        sum = sum+num[index];
+        
+    }
+    //console.log(sum);
+ console.log(`num1 is ${num1} , num2 is ${num2} & array of rest parametre num is ${num} & total sum in ${sum}`);
+ 
+}
+add(5,6);
+add(9,5,6);
+add(78,90,51,68);
+add(89,90,45,90,51,68);
---
+++ 19_selfInvockingFunction.js
@@ -0,0 +1,11 @@
+// Self invoking function or IIFE - Immediately invoked function expression
+
+( function(){
+
+    console.log(`inside display function..`);
+
+} )();
+
+
+
+
